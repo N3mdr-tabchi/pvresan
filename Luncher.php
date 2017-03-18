1e69c6d8d6bde7f20f8aa9a42d365757307a1b22
N3mdr-tabchi <n3mdr-tabchi@12storage.com>
2017-03-18 18:16:17 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,36 +1,21 @@
 <?php
 /*
 سورس اصلی ربات پی وی رسان
-@PvResanBot
+@CoderTeam_Robot
 دست هیچ بنی بشری نیست و نخواهد بود
 
 این سورس ای هم که داخل این گیت هاب هست
 سورس ورژن یک پی وی رسان هست
 و پی وی رسان هم اکنون از سورس ورژن های بالاتر استفاده میکند
 
-تیم برنامه نویسی FTC
+تیم برنامه نویسی CoderTeam
 با آدرس سایت
-https://feelthecode.com
+https://CoderTeam.ir
 
-اولین سازنده ربات پیام رسان فارسی در تلگرام
-و تنها برنامه نویس ربات پی وی رسان می باشد
+site: https://CoderTeam.ir
+Telegram Bot: https://telegram.me/CoderTeam_Robot
+Telegram Channel: https://t.me/CodersTeam
 
-لطفا از هیچکس هیچ سورس و یا آموزش تقلبی را خریداری نکنید
-نرم افزار های موجود در سطح اینترنت نیز
-برای ساخت ربات پیام رسان
-کلاه برداری بیش نیستند
-
-در صورت مشاهده این گونه افراد
-حتما با استفاده از فرم تماس با ما
-موجود در وب سایت ما
-یا از طریق تماس با کارشناسان ما
-ما را در جریان قرار دهید تا پیگیری های لازم
-صورت پذیرد
-
-site: https://feelthecode.com
-Telegram Bot: https://telegram.me/PvResanBot
-Telegram Channel: https://telegram.me/PvResan
-
-یا علی خدا قوت
+#sadra_khoffash
 */
 ?>
